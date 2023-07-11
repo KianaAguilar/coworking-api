3febe6461e4bf710e223b520a049a675c5ac731f
Kiana <kbermudez@asap-consulting.net>
2023-07-10 22:23:58 -0300

    agrego .gitignore al proyecto

---
+++ src/test/java/coworkingapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package coworkingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
